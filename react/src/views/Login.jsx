6eaf4fdf543850f09af1513c81136ce8dc95eca7
fabio vige <fabiovige@gmail.com>
2023-07-19 13:07:23 -0300

    criando trela de login e cadastro

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,7 +1,24 @@
+import {Link} from "react-router-dom";
+
 export default function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+
+  const onSubmit = (ev) => {
+    ev.preventDefault()
+  }
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Entre na sua conta</h1>
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <button className="btn btn-block">Entrar</button>
+          <p className="message">
+            Não tem conta? <Link to="/signup">Increva-se</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  )
 }
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom";
+
 export default function Signup() {
-    return (
-        <div>
-            Signup
-        </div>
-    )
+
+  const onSubmit = (ev) => {
+    ev.preventDefault()
+  }
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Inscreva-se</h1>
+          <input type="text" placeholder="Nome completo" />
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <input type="password" placeholder="Confirme a senha" />
+          <button className="btn btn-block">Inscrever</button>
+          <p className="message">
+            Possui conta? <Link to="/login">Entrar</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  )
 }
